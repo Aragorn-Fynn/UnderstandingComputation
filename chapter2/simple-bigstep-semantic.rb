b6ff9b786f4a0d509d2fdf35ebfaefe2186ae3d2
冯程飞 <553038170@qq.com>
2021-04-20 16:54:50 +0800

    实现simple语言的大步语义

---
+++ chapter2/simple-bigstep-semantic.rb
@@ -0,0 +1,84 @@
+#大步语义： 为了对一个更大的表达式求值， 我们要对所有比它小的子表达式求值， 然后把结果结合起来得到最终答案
+class Number
+    def evaluate(environment)
+        self
+    end
+end
+
+class Boolean
+    def evaluate(environment)
+        self
+    end
+end
+
+class Variable
+    def evaluate(environment)
+        environment[name]
+    end
+end
+
+class Add
+    def evaluate(environment)
+        Number.new(left.evaluate(environment).value + right.evaluate(environment).value)
+    end
+end
+
+class Multiply
+    def evaluate(environment)
+        Number.new(left.evaluate(environment).value * right.evaluate(environment).value)
+    end
+end
+
+class LessThan
+    def evaluate(environment)
+        Boolean.new(left.evaluate(environment).value < right.evaluate(environment).value)
+    end
+end
+
+class Assign
+    def evaluate(environment)
+        environment.merge({ name => expression.evaluate(environment) })
+    end
+end
+
+class DoNothing
+    def evaluate(environment)
+        environment
+    end
+end
+
+class If
+    def evaluate(environment)
+        case condition.evaluate(environment)
+        when Boolean.new(true)
+            consequence.evaluate(environment)
+        when Boolean.new(false)
+            alternative.evaluate(environment)
+        end
+    end
+end
+
+class Sequence
+    def evaluate(environment)
+        second.evaluate(first.evaluate(environment))
+    end
+end
+
+class While
+    def evaluate(environment)
+        case condition.evaluate(environment)
+        when Boolean.new(true)
+            evaluate(body.evaluate(environment))
+        when Boolean.new(false)
+            environment
+        end
+    end
+end
+
+#test
+statement =
+    While.new(
+        LessThan.new(Variable.new(:x), Number.new(5)),
+        Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
+    )
+statement.evaluate({ x: Number.new(1) })
