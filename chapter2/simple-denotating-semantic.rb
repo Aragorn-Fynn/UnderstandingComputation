ed9d4f9b262400aac930328590202af60901557d
冯程飞 <553038170@qq.com>
2021-04-21 15:54:26 +0800

    生成AST

--- chapter2/simple-denotating-semantic.rb
+++ chapter2/simple-denotating-semantic.rb
@@ -1,84 +1,90 @@
-#指称语义， 将一个语言翻译为另一个语言
+# 指称语义， 将一个语言翻译为另一个语言
 
 class Number
-    def to_ruby
-        "-> e { #{value.inspect} }"
-    end
+  def to_ruby
+    "-> e { #{value.inspect} }"
+  end
 end
 
 class Boolean
-    def to_ruby
-        "-> e { #{value.inspect} }"
-    end
+  def to_ruby
+    "-> e { #{value.inspect} }"
+  end
 end
 
 class Variable
-    def to_ruby
-        "-> e { e[#{name.inspect}] }"
-    end
+  def to_ruby
+    "-> e { e[#{name.inspect}] }"
+  end
 end
 
 class Add
-    def to_ruby
-        "-> e { (#{left.to_ruby}).call(e) + (#{right.to_ruby}).call(e) }"
-    end
+  def to_ruby
+    "-> e { (#{left.to_ruby}).call(e) + (#{right.to_ruby}).call(e) }"
+  end
 end
 
 class Multiply
-    def to_ruby
-        "-> e { (#{left.to_ruby}).call(e) * (#{right.to_ruby}).call(e) }"
-    end
+  def to_ruby
+    "-> e { (#{left.to_ruby}).call(e) * (#{right.to_ruby}).call(e) }"
+  end
 end
 
 class LessThan
-    def to_ruby
-        "-> e { (#{left.to_ruby}).call(e) < (#{right.to_ruby}).call(e) }"
-    end
+  def to_ruby
+    "-> e { (#{left.to_ruby}).call(e) < (#{right.to_ruby}).call(e) }"
+  end
 end
 
 class Assign
-    def to_ruby
-        "-> e { e.merge({ #{name.inspect} => (#{expression.to_ruby}).call(e) }) }"
-    end
+  def to_ruby
+    "-> e { e.merge({ #{name.inspect} => (#{expression.to_ruby}).call(e) }) }"
+  end
 end
 
 class DoNothing
-    def to_ruby
-        '-> e { e }'
-    end
+  def to_ruby
+    '-> e { e }'
+  end
 end
 
 class If
-    def to_ruby
-        "-> e { if (#{condition.to_ruby}).call(e)" +
-        " then (#{consequence.to_ruby}).call(e)" +
-        " else (#{alternative.to_ruby}).call(e)" +
-        " end }"
-    end
+  def to_ruby
+    "-> e { if (#{condition.to_ruby}).call(e)" +
+      " then (#{consequence.to_ruby}).call(e)" +
+      " else (#{alternative.to_ruby}).call(e)" +
+      ' end }'
+  end
 end
 
 class Sequence
-    def to_ruby
-        "-> e { (#{second.to_ruby}).call((#{first.to_ruby}).call(e)) }"
-    end
+  def to_ruby
+    "-> e { (#{second.to_ruby}).call((#{first.to_ruby}).call(e)) }"
+  end
 end
 
 class While
-    def to_ruby
-        "-> e {" +
-        " while (#{condition.to_ruby}).call(e); e = (#{body.to_ruby}).call(e); end;" +
-        " e" +
-        " }"
-    end
+  def to_ruby
+    '-> e {' +
+      " while (#{condition.to_ruby}).call(e); e = (#{body.to_ruby}).call(e); end;" +
+      ' e' +
+      ' }'
+  end
 end
 
-#test
+# test
 statement =
-    While.new(
-        LessThan.new(Variable.new(:x), Number.new(5)),
-        Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
-    )
-#转换成ruby程序
+  While.new(
+    LessThan.new(Variable.new(:x), Number.new(5)),
+    Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
+  )
+# 转换成ruby程序
 proc = eval(statement.to_ruby)
-#执行
+# 执行
 proc.call({ x: 1 })
+
+require 'treetop'
+Treetop.load('chapter2/simple')
+parse_tree = SimpleParser.new.parse('while (x < 5) { x = x * 3 }')
+statement = parse_tree.to_ast
+eval(statement.to_ruby).call({ x: Number.new(1)})
